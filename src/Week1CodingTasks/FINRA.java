c8517348834a35fa58aa55a03e96ce505331b4c1
ShukhratR <shukhrat0906@gmail.com>
2023-09-27 13:14:27 -0700

    FINRA Task task solution update

--- src/Week1CodingTasks/FINRA.java
+++ src/Week1CodingTasks/FINRA.java
@@ -10,7 +10,7 @@ public class FINRA {
 
     public static void Finra(int number){
 
-        for (int i = 1; i <= 30 ; i++) {
+        for (int i = 1; i <= number ; i++) {
 
             if (i % 3 == 0 && i % 5 == 0){
                 System.out.print("FINRA ");
