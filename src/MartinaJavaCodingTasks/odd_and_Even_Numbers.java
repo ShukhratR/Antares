533650c982fc5c64337e9562c52ea37a128a6a69
montetina <martinaradovic1992@gmail.com>
2023-09-29 00:54:28 -0700

    create odd and even class

--- src/MartinaJavaCodingTasks/odd_and_Even_Numbers.java
+++ src/MartinaJavaCodingTasks/odd_and_Even_Numbers.java
@@ -5,12 +5,26 @@ public class odd_and_Even_Numbers {
 
     public static void main(String[] args) {
 
+        System.out.println(identify(11));
 
+    }
 
+    public static String identify(int number){
+        if (number % 2 == 0){
+            return "Even";
+        }else {
+            return "Odd";
+        }
 
 
     }
+
 }
+     /*Write a method which can identifies given number is even or odd;
+    ex:
+    identify(5) --> "Odd";
+    identify(6) --> "Even";
+    */
 
 
-// Write a method which can identifies given number is even or odd
+
