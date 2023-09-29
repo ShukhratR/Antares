b5bff42f0c9106961640b4e0b451307570fd15e4
montetina <martinaradovic1992@gmail.com>
2023-09-29 11:12:44 -0700

    create divide without / operator

---
+++ src/MartinaJavaCodingTasks/divide_without__devision_operator.java
@@ -0,0 +1,13 @@
+package MartinaJavaCodingTasks;
+
+public class divide_without__devision_operator {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
+ // Write a method that can divide 2 numbers without using / operator
