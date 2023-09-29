ab0aca8d5703a1a69c7a428de491174c1774816c
montetina <martinaradovic1992@gmail.com>
2023-09-29 00:31:04 -0700

    create odd and even class

---
+++ src/MartinaJavaCodingTasks/odd_and_Even_Numbers.java
@@ -0,0 +1,16 @@
+package MartinaJavaCodingTasks;
+
+public class odd_and_Even_Numbers {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
+
+
+// Write a method which can identifies given number is even or odd
