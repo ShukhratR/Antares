c7a21b0be3b25f07aa9850ae17e4f54154ad8eba
montetina <martinaradovic1992@gmail.com>
2023-10-01 00:14:03 -0700

    finra class finished

--- src/MartinaJavaCodingTasks/finra.java
+++ src/MartinaJavaCodingTasks/finra.java
@@ -1,7 +1,38 @@
 package MartinaJavaCodingTasks;
 
 public class finra {
+
+
+    public static void main(String[] args) {
+        printFinra();
+
+    }
+    public static void printFinra(){
+
+        for (int num = 1; num <= 30; num++){
+
+            if (num % 3 == 0 && num % 5 == 0){
+                System.out.println("FINRA");
+            } else if (num % 3 == 0) {
+                System.out.println("FIN");
+            } else if (num % 5 == 0){
+                System.out.println("RA");
+            } else {
+                System.out.println(num);
+            }
+        }
+
+    }
+
+
 }
+
+
+
+
+
+
+
 
 
 /*
